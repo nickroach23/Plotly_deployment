f2c492143637e3cdc36ff64a1d3cca27df33fefd
nickroach23 <64110317+nickroach23@users.noreply.github.com>
2021-09-26 20:49:33 -0400

    file

--- BellyButton_bubble_chart_starter_code.js
+++ BellyButton_bubble_chart_starter_code.js
@@ -1,24 +1,52 @@
-// Bar and Bubble charts
-// Create the buildCharts function.
-function buildCharts(sample) {
-  // Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+// // Bar and Bubble charts
+// // Create the buildCharts function.
+// function buildCharts(sample) {
+//   // Use d3.json to load and retrieve the samples.json file 
+//   d3.json("samples.json").then((data) => {
     
 
-    // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot(); 
+//     // Deliverable 1 Step 10. Use Plotly to plot the data with the layout. 
+//     Plotly.newPlot(); 
 
-    // 1. Create the trace for the bubble chart.
-    var bubbleData = [
+//     // 1. Create the trace for the bubble chart.
+//     var bubbleData = [
    
-    ];
+//     ];
 
-    // 2. Create the layout for the bubble chart.
-    var bubbleLayout = {
+//     // 2. Create the layout for the bubble chart.
+//     var bubbleLayout = {
       
-    };
+//     };
 
-    // 3. Use Plotly to plot the data with the layout.
-    Plotly.newPlot(); 
-  });
+//     // 3. Use Plotly to plot the data with the layout.
+//     Plotly.newPlot(); 
+//   });
+// }
+
+var bubbleData = [{
+  x: otu_id,
+  y: sample_value,
+  text: otu_label,
+  mode: 'markers',
+  marker:{
+    size: sample_value,
+    color: otu_id,
+    colorscale: "Earth"
+
+  }
+
 }
+];
+
+// 2. Create the layout for the bubble chart.
+var bubbleLayout = {
+  title: "Bacteria Culture Per Sample",
+  xaxis: {
+    title: {text:"OTU ID"}
+  }
+
+
+};
+
+// 3. Use Plotly to plot the data with the layout.
+Plotly.newPlot('bubble', bubbleData, bubbleLayout);
